6fe6024bebf631554481b523af4995e1072cdbad
Olesya <oltekuteva@gmail.com>
2023-01-07 13:31:22 -0600

    add main method

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -1,4 +1,8 @@
 package firstPackage;
 
 public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("First Class");
+    }
+
 }
--- src/firstPackage/SecondClass.java
+++ src/firstPackage/SecondClass.java
@@ -1,4 +1,7 @@
 package firstPackage;
 
 public class SecondClass {
+    public static void main(String[] args) {
+
+    }
 }
